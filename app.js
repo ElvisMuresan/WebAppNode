bda7b9fd11d34cfed7850dbac125debd98968006
ElvisMuresan <elvis.e.muresan@gmail.com>
2023-07-03 21:53:42 +0300

    Tried to connect to the mongodb, but failed

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
@@ -8,6 +7,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3000;
 const app = express();
 const sessionsRouter = require("./src/routers/sesionsRouter");
+const adminRouter = require("./src/routers/adminRouter");
 
 app.use(morgan("tiny"));
 app.use(express.static(path.join(__dirname, "/public/")));
@@ -16,6 +16,7 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use("/sessions", sessionsRouter);
+app.use("/admin", adminRouter);
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Globomantics", data: ["a", "b", "c"] });
---
+++ src/routers/adminRouter.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const debug = require("debug")("app:adminRouter");
+const { MongoClient } = require("mongodb");
+const sessions = require("../data/sessions.json");
+
+const adminRouter = express.Router();
+
+adminRouter.route("/").get((req, res) => {
+  const url =
+    "mongodb+srv://elvisemuresan:<Barcelona3!@!>@globomantics.njydi5q.mongodb.net/?retryWrites=true&w=majority";
+  const dbName = "globomantics";
+
+  (async function mongo() {
+    let client;
+    try {
+      client = await MongoClient.connect(url);
+      debug("Connect to the mongo DB");
+
+      const db = client.db(dbName);
+
+      const response = await db.collection("sessions").insertMany(sessions);
+      res.json(response);
+    } catch (error) {
+      debug(error.stack);
+    }
+  })();
+});
+
+module.exports = adminRouter;
